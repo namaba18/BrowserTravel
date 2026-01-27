8bc3aeec1a477ddb37e7d5b9dcf2445cd0da2ab8
Natalia Mantilla <natalia.mantilla@sourcecol.com>
2026-01-27 14:35:33 -0500

    Create Db connection

--- Infrastructure/Data/AppDbContext.cs
+++ Infrastructure/Data/AppDbContext.cs
@@ -1,7 +1,43 @@
-﻿namespace Infrastructure.Data
-{
-    public class AppDbContext
-    {
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
+namespace Infrastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+        {
+        }
+
+        public DbSet<Customer> Customers => Set<Customer>();
+        public DbSet<Car> Cars => Set<Car>();
+        public DbSet<Reservation> Reservations => Set<Reservation>();
+        public DbSet<Location> Locations => Set<Location>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Car>(entity =>
+            {
+                entity.HasKey(e => e.Id);                
+            });
+            modelBuilder.Entity<Customer>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.FirstName).IsRequired().HasMaxLength(100);
+                entity.Property(e => e.LastName).IsRequired().HasMaxLength(100);
+                entity.Property(e => e.Email).IsRequired().HasMaxLength(100);
+                entity.Property(e => e.PhoneNumber).IsRequired().HasMaxLength(15);
+            });
+            modelBuilder.Entity<Location>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+            });
+            modelBuilder.Entity<Reservation>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+            });
+        }
     }
 }
--- Web/Program.cs
+++ Web/Program.cs
@@ -1,6 +1,12 @@
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
